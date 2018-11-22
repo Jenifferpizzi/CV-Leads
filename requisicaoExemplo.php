36aeb0df6149ffd0e05cb7ceb936659b93add643
Breno Pinho Menezes <brenop@ALFAMA.COM>
2018-11-22 09:38:52 -0300

    Arquivos de dados e requisição

---
+++ requisicaoExemplo.php
@@ -0,0 +1,32 @@
+<?php
+
+$usuario = '[SEU_EMAIL]';
+$senha = '[SUA_SENHA]';
+$url = '[URL_DO_SEU_CONSTRUTOR]/api/cvio/lead';
+
+$dadosLead = array(
+	'acao' => 'salvar_editar',
+    'nome' => 'Nivaldo Oliva Segundo',
+    'email' => 'nivaldoolivasegundo@gmail.com',
+    'telefone' => '(79) 98833-0011',
+    'modulo' => 'gestor',
+    'facebook' => 'https://pt-br.facebook.com/alfamaweb/',
+    'twitter' => 'https://twitter.com/alfamaweb',
+    'instagram' => 'https://www.instagram.com/alfamaweb',
+    'url_avatar' => 'https://alfamaweb.com.br/assets/img/logo_alfama_blog.png',
+);
+
+$dadosLead = json_encode($dadosLead);
+
+$cabecalho = array(
+	'email: ' . $usuario,
+	'senha: ' . $senha,
+	'Content-Type: application/json',
+	'Content-Length: ' . strlen($dadosLead)
+);
+
+$curlHandler = curl_init($url);
+curl_setopt($curlHandler, CURLOPT_POSTFIELDS, $dadosLead);
+curl_setopt($curlHandler, CURLOPT_RETURNTRANSFER, TRUE);
+curl_setopt($curlHandler, CURLOPT_HTTPHEADER, $cabecalho);
+$retorno = curl_exec($curlHandler);
